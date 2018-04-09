7871782073681511a63089e5812e584baa893019
yoksha <ani.muruga@gmail.com>
2018-04-09 13:28:15 -0400

    java proj

---
+++ src/main/java/com/ani/java/SampleSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.ani.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootApplication.class, args);
+		
+	}
+
+}
